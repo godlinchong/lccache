38832e0e727b540635a92a06bb87d828f14eeec5
Chong Lin <chong.lin@sparkxmarketing.com>
2021-02-19 18:37:06 +0800

    增加group相关接口

--- lccache.go
+++ lccache.go
@@ -1,6 +1,10 @@
 package lccache
 
-import "sync"
+import (
+	"fmt"
+	"log"
+	"sync"
+)
 
 // 缓存组
 type Group struct {
@@ -51,4 +55,37 @@ func GetGroup(name string) *Group {
 	g := groups[name]
 	mu.RUnlock()
 	return g
+}
+
+// group获取缓存
+func (g *Group) Get(key string) (ByteView, error) {
+	if key == "" {
+		return ByteView{}, fmt.Errorf("key is required")
+	}
+	if v, ok := g.mainCache.get(key); ok {
+		log.Println("[GeeCache] hit")
+		return v, nil
+	}
+	return g.load(key)
+}
+
+// 加载数据
+func (g *Group) load(key string) (ByteView, error) {
+	return g.getLocally(key)
+}
+
+// 加载本地数据
+func (g *Group) getLocally(key string) (ByteView, error) {
+	bytes, err := g.getter.Get(key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	value := ByteView{b: cloneBytes(bytes)}
+	g.populateCache(key, value)
+	return value, nil
+}
+
+// 加入缓存
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.add(key, value)
 }
